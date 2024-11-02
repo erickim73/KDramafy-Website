e4bcac18eeb8aebad551da301571f0ef76280fcc
Eric Kim <seyoon2006@gmail.com>
2024-11-02 00:42:49 -0500

    updated navbar and home page

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,11 +2,11 @@ import {Link} from "react-router-dom"
 
 export const Navbar = () => {
     return (
-        <nav className = "relative flex w-full items-center justify-between bg-[#0f1d21] lg:py-6">
+        <nav className = "sticky top-0 z-50 flex w-full items-center justify-between bg-[#091217] lg:py-6">
             <div className = "flex items-center">
                 {/* Website name */}
                 <Link to = "/"> 
-                    <span className = "text-[27px] ml-2 font-montserrat text-white font-extrabold px-4 py-4">
+                    <span className = "text-[29px] ml-2 font-montserrat text-white font-black px-4 py-4">
                         KDramafy
                     </span> 
                 </Link>
@@ -15,7 +15,7 @@ export const Navbar = () => {
             <div className = "flex text-right">
                     <Link to = "/signup"> 
                         <button 
-                            className = "text-white rounded-xl font-medium font-montserrat border border-white px-4 py-2 mr-4 transition duration-300 ease-in-out transform hover:text-[#ffffff] hover:scale-105"
+                            className = "text-white rounded-xl font-medium font-montserrat border border-white px-4 py-2 mr-4 transition duration-300 ease-in-out transform hover:text-[#ffffff] hover:scale-110"
                             style = {{
                                 background: "linear-gradient(180deg, #e2e8ff00, #e2e8ff1f)", 
                                 padding: "8px 16px"
@@ -26,7 +26,7 @@ export const Navbar = () => {
                     </Link>
                     <Link to = "/login"> 
                         <button 
-                            className = "text-white rounded-xl font-medium border font-montserrat border-white px-4 py-2 mr-5 transition duration-300 ease-in-out transform hover:text-[#ffffff] hover:scale-105"
+                            className = "text-white rounded-xl font-medium border font-montserrat border-white px-4 py-2 mr-5 transition duration-300 ease-in-out transform hover:text-[#ffffff] hover:scale-110"
                             style = {{
                                 background: "linear-gradient(180deg, #e2e8ff00, #e2e8ff1f)", 
                                 padding: "8px 16px"
--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,10 +1,48 @@
+import { Link } from "react-router-dom"
+
 export const Home = () => {
     return (
-        <div className = "bg-[#091215] min-h-screen flex flex-col items-center justify-center text-white">
-            <h3>Discover Your Perfect K-Drama Today</h3>
-            <h1>KDramafy, a new destination for all K-Drama lovers</h1>
-            <h4>Unlimited K-Drama recommendations, personalized K-Drama portfoilo, and more</h4>
-            <button>Get Started!</button>
+        <div className = "bg-[#081014] min-h-screen flex flex-col items-center justify-center text-white p-4 relative">
+            {/* button for slogan */}
+            <button className="relative p-[2px] rounded-full bg-gradient-to-r from-[#6a5acd] via-[#836ab6] to-[#b293d3] mb-10 -mt-32">
+                <span className = "block bg-[#081014] rounded-full px-6 py-2">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#6a5acd] via-[#836ab6] to-[#b293d3] font-semibold">
+                        Discover Your Perfect K-Drama Today
+                    </span>
+                </span>
+            </button>
+
+            {/* main message */}
+            <div className = "text-6xl font-bold text-center font-montserrat space-y-6 mb-8 w-11/12 ">
+                <div className = "text-gray-200">
+                    KDramafy, a new destination
+                </div>
+                <div className = "bg-gradient-to-r from-[#6a5acd] via-[#836ab6] to-[#b293d3] inline-block text-transparent bg-clip-text py-2">
+                    for all K-Drama lovers
+                </div>
+            </div>
+            
+            {/* second message about KDramafy */}
+            <div className = "text-lg text-center font-montserrat space-y-1 mb-6 text-gray-400">
+                <div >
+                    Discover Your Perfect K-Drama Match: Unlimited K-Drama
+                </div>
+                <div>
+                     Recommendations, Custom Watchlists, and More
+                </div>
+            </div>
+
+            {/* Get Started Button */}
+            <div className = "font-montserrat rounded-xl font-medium px-4 py-2 mr-4 transition duration-300 ease-in-out transform hover:scale-110 bg-gradient-to-r from-[#6a5acd] via-[#836ab6] to-[#b293d3]">
+                <Link to = "/signup">
+                    <button>
+                        Get Started
+                    </button>
+                </Link>
+            </div>
+
+            
+
         </div>
     )
 }
