8fee1482d4265bff887a00b675762d1a6ea86c60
Eric Kim <seyoon2006@gmail.com>
2024-12-15 15:00:48 -0600

    updated ui

--- src/pages/onboarding/Onboarding.tsx
+++ src/pages/onboarding/Onboarding.tsx
@@ -136,64 +136,64 @@ export const Onboarding = () => {
 
     return (
     // release year, number of episodes, rating, number of ratings, genre preference
-        <div className = "bg-[black] overflow-hidden min-h-screen flex flex-col items-center justify-center text-white p-4 relative">
+        <div className = "bg-[black] overflow-y-auto min-h-screen flex flex-col items-center justify-center text-white p-4 relative">
             <TracingBeam>
-                <form onSubmit = {handleSubmit(onSubmit)} className = "bg-[black] w-full max-w-xl p-8 mx-auto shadow-2xl rounded-2xl">
+                <form onSubmit = {handleSubmit(onSubmit)} className = "bg-[black] w-full max-w-xl p-8 sm:p-8 mx-auto shadow-2xl rounded-2xl">
                     <h1 className="mb-8 text-3xl font-bold text-center">K-Drama Preferences</h1>
                     {/* Release Year Range */}
                     <div className="mb-8">
-                    <label className="block mb-2 text-xl font-semibold">Release Year Range</label>
-                    <div className="flex flex-wrap gap-2 mb-4">
-                        {Object.entries(predefinedCategories.releaseYear).map(([card, values]) => {
-                        const [category, range] = card.split(' (');
-                        const isStarred = card.includes("New");
-                        return (
-                            <motion.button
-                            type="button"
-                            key={card}
-                            onClick={() => toggleCard("releaseYear", card, values)}
-                            className={`px-[26px] py-2.5 rounded-lg ${
-                                selectedCards.releaseYear.includes(card)
-                                ? "bg-gradient-to-r from-[#6a5acd] via-[#836ab6] to-[#b386e4] text-white"
-                                : "bg-gray-700 text-gray-200"
-                            } relative transition-all duration-200 ease-in-out text-sm flex flex-col items-center justify-center min-w-[100px]`}
-                            whileHover={{ scale: 1.05 }}
-                            whileTap={{ scale: 0.95 }}
-                            >
-                                {/* Star on top-right corner */}
-                                {isStarred && (
-                                    <span className="absolute z-10 text-xs font-bold text-yellow-400 top-1 right-1">
-                                        ⭐
-                                    </span>
-                                )}
-                                <span className="font-semibold">{category}</span>
-                                <span className="text-xs">({range}</span>
-                            </motion.button>
-                        );
-                        })}
-                    </div>
-                    <div className="flex space-x-4">
-                        <div className="flex flex-col w-1/2">
-                        <label className="mb-1 text-sm">Min Year</label>
-                        <input
-                            type="text"
-                            {...register("minYear", {
-                            required: "Required",
-                            validate: (value) =>
-                                parseInt(value) <= parseInt(watch("maxYear"))
+                        <label className="block mb-2 text-xl font-semibold">Release Year Range</label>
+                        <div className="flex flex-wrap gap-2 mb-4">
+                            {Object.entries(predefinedCategories.releaseYear).map(([card, values]) => {
+                            const [category, range] = card.split(' (');
+                            const isStarred = card.includes("New");
+                            return (
+                                <motion.button
+                                type="button"
+                                key={card}
+                                onClick={() => toggleCard("releaseYear", card, values)}
+                                className={`w-full sm:w-auto px-[26px] py-2.5 rounded-lg ${
+                                    selectedCards.releaseYear.includes(card)
+                                    ? "bg-gradient-to-r from-[#6a5acd] via-[#836ab6] to-[#b386e4] text-white"
+                                    : "bg-gray-700 text-gray-200"
+                                } relative transition-all duration-200 ease-in-out text-sm flex flex-col items-center justify-center min-w-[100px]`}
+                                whileHover={{ scale: 1.05 }}
+                                whileTap={{ scale: 0.95 }}
+                                >
+                                    {/* Star on top-right corner */}
+                                    {isStarred && (
+                                        <span className="absolute z-10 text-xs font-bold text-yellow-400 top-1 right-1">
+                                            ⭐
+                                        </span>
+                                    )}
+                                    <span className="font-semibold">{category}</span>
+                                    <span className="text-xs">({range}</span>
+                                </motion.button>
+                            );
                             })}
-                            className="px-3 py-2 text-white bg-gray-700 rounded-lg"
-                        />
                         </div>
-                        <div className="flex flex-col w-1/2">
-                        <label className="mb-1 text-sm">Max Year</label>
-                        <input
-                            type="text"
-                            {...register("maxYear", { required: "Required" })}
-                            className="px-3 py-2 text-white bg-gray-700 rounded-lg"
-                        />
+                        <div className="flex space-x-4">
+                            <div className="flex flex-col w-1/2">
+                            <label className="mb-1 text-sm">Min Year</label>
+                            <input
+                                type="text"
+                                {...register("minYear", {
+                                required: "Required",
+                                validate: (value) =>
+                                    parseInt(value) <= parseInt(watch("maxYear"))
+                                })}
+                                className="px-3 py-2 text-white bg-gray-700 rounded-lg"
+                            />
+                            </div>
+                            <div className="flex flex-col w-1/2">
+                            <label className="mb-1 text-sm">Max Year</label>
+                            <input
+                                type="text"
+                                {...register("maxYear", { required: "Required" })}
+                                className="px-3 py-2 text-white bg-gray-700 rounded-lg"
+                            />
+                            </div>
                         </div>
-                    </div>
                         
                     </div>
                     {/* Number of Episodes */}
@@ -208,7 +208,7 @@ export const Onboarding = () => {
                             type="button"
                             key={card}
                             onClick={() => toggleCard('episodes', card, values)}
-                            className={`px-[35px] py-2.5 rounded-lg ${
+                            className={`w-full sm:w-auto px-[35px] py-2.5 rounded-lg ${
                                 selectedCards.episodes.includes(card)
                                 ? "bg-gradient-to-r from-[#6a5acd] via-[#836ab6] to-[#b386e4] text-white"
                                 : "bg-gray-700 text-gray-200"
@@ -264,7 +264,7 @@ export const Onboarding = () => {
                             type="button"
                             key={card}
                             onClick={() => toggleCard('numRatings', card, values)}
-                            className={`px-[22px] py-2.5 rounded-lg ${
+                            className={`w-full sm:w-auto px-[22px] py-2.5 rounded-lg ${
                                 selectedCards.numRatings.includes(card)
                                 ? "bg-gradient-to-r from-[#6a5acd] via-[#836ab6] to-[#b386e4] text-white"
                                 : "bg-gray-700 text-gray-200"
